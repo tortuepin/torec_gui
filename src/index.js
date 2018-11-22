efa2949417a65c5768d948c3b5d2772a3f5e16ee
tortuepin <kameyasu119@gmail.com>
2018-11-22 14:31:43 +0900

    Install react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 
 
 render(
-  <div>Hello World</div>,
+  <div>
+    <App/>
+  </div>
   document.getElementById('root')
 )
